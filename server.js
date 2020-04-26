4ced90f86c04b6969fbbc8127ceae2c99cddb6c4
dbeccaria826 <beccdaniel@gmail.com>
2020-04-26 13:09:07 -0400

    added mongodb connection and config folder for back-end mongo connects successfully

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome to contactkeeper api...'})
 })
+
+// Connect DB
+
+connectDB();
 
 // Define Routes
 
