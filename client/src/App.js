a002a3e0c600e79caf5c32054cf456b02747b103
dbeccaria826 <beccdaniel@gmail.com>
2020-04-27 13:30:18 -0400

    added working navbar and routes to App.js and Navbar.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Navbar from './components/Layout/Navbar';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+       <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+   
   );
 }
 
---
+++ client/src/components/Pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About This App</h1> 
+            <p className="p-1">
+                Simple app for keeping and managing contacts
+            </p>
+            <p className="bg-dark p"><strong>Version: 1.0.0</strong></p>
+        </div>
+    )
+};
+
+export default About;
