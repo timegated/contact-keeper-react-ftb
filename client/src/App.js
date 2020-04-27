79a22967d8a2c23272216fc63e02229cf094f591
dbeccaria826 <beccdaniel@gmail.com>
2020-04-27 13:18:49 -0400

    added client folder with cra, cleaned up client and added navbar component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Layout/Navbar';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`)
