fd6dab25bff8b4572def99f236ad71af75fe6cfe
dbeccaria826 <beccdaniel@gmail.com>
2020-04-30 12:14:00 -0400

    added alert context and auth context for user login and registration.

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,36 @@ import React, { Fragment } from 'react';
 import Navbar from './components/Layout/Navbar';
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-       <Router>
-       <Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/about" component={About}/>
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
-}
+};
 
 export default App;
--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -16,9 +16,15 @@ const Navbar = ({title, icon}) => {
                 <li>
                     <Link to="/about">About</Link>
                 </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
             </ul>
         </div>
-    )
+    );
 };
 
 Navbar.propTypes = {
