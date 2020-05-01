b3797b0daaec2aaa857b946378b45648e8eb8b63
dbeccaria826 <beccdaniel@gmail.com>
2020-04-30 20:57:49 -0400

    removed test route in server.js

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const connectDB = require('./config/db');
 const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to contactkeeper api...'})
-})
-
 // Init Middleware
 
 app.use(express.json({ extended: false }));
@@ -23,7 +19,7 @@ app.use('/api/contacts', require('./routes/contacts'))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
